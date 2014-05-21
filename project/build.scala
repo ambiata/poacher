8120ebffcdde68f995dd596c83c20a03efb85220
Eric Torreborre <etorreborre@yahoo.com>
2014-05-21 17:06:20 +1000

    first import

---
+++ project/build.scala
@@ -0,0 +1,56 @@
+import sbt._
+import Keys._
+import sbt.KeyRanks._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+import com.ambiata.promulgate.project.ProjectPlugin._
+
+object build extends Build {
+  type Settings = Def.Setting[_]
+
+  lazy val chassflad = Project(
+    id = "chassflad"
+  , base = file(".")
+  , settings = 
+    standardSettings ++ 
+    promulgate.library(s"com.ambiata.chassflad", "ambiata-oss") ++
+    Seq[Settings](libraryDependencies ++= depend.scalaz ++ depend.mundane ++ depend.saws ++ depend.scoobi(version.value) ++ depend.specs2)
+  )
+
+  lazy val standardSettings = Defaults.defaultSettings ++
+                              projectSettings          ++
+                              compilationSettings      ++
+                              testingSettings          ++
+                              Seq[Settings](
+                                resolvers := depend.resolvers
+                              )
+
+  lazy val projectSettings: Seq[Settings] = Seq(
+    name := "chassflad"
+  , version in ThisBuild := s"""1.0.0-${Option(System.getenv("HADOOP_VERSION")).getOrElse("cdh5")}"""
+  , organization := "com.ambiata"
+  , scalaVersion := "2.10.4"
+  , fork in run  := true
+  )
+
+  lazy val compilationSettings: Seq[Settings] = Seq(
+    javaOptions ++= Seq("-Xmx3G", "-Xms512m", "-Xss4m")
+  , javacOptions ++= Seq("-source", "1.6", "-target", "1.6")
+  , maxErrors := 20
+  , scalacOptions ++= Seq("-target:jvm-1.6", "-deprecation", "-unchecked", "-feature", "-language:_", "-Ywarn-all", "-Xlint", "-Xfatal-warnings", "-Yinline-warnings")
+  )
+
+  lazy val testingSettings: Seq[Settings] = Seq(
+    initialCommands in console := "import org.specs2._"
+  , logBuffered := false
+  , cancelable := true
+  , fork in test := true
+  , javaOptions += "-Xmx3G"
+  )
+
+  lazy val buildAssemblySettings: Seq[Settings] = Seq(
+    artifact in (Compile, assembly) ~= { art =>
+      art.copy(`classifier` = Some("assembly"))
+    }
+  ) ++ addArtifact(artifact in (Compile, assembly), assembly)
+}
---
+++ project/depend.scala
@@ -0,0 +1,41 @@
+import sbt._
+import Keys._
+
+object depend {
+  val scalaz    = Seq("org.scalaz"           %% "scalaz-core"     % "7.0.6",
+                      "org.scalaz"           %% "scalaz-effect"   % "7.0.6")
+  val scopt     = Seq("com.github.scopt"     %% "scopt"           % "3.2.0")
+  val trove     = Seq("trove"                %  "trove"           % "1.0.2")
+  val joda      = Seq("joda-time"            %  "joda-time"       % "2.1",
+                      "org.joda"             %  "joda-convert"    % "1.1")
+  val specs2    = Seq("org.specs2"           %% "specs2-core",
+                      "org.specs2"           %% "specs2-junit",
+                      "org.specs2"           %% "specs2-html",
+                      "org.specs2"           %% "specs2-matcher-extra",
+                      "org.specs2"           %% "specs2-scalacheck").map(_ % "2.3.11")
+  val commonsio = Seq("commons-io"           %  "commons-io"      % "2.4")
+  val thrift    = Seq("org.apache.thrift"    %  "libthrift"       % "0.9.1")
+  val saws      = Seq("com.ambiata"          %% "saws"            % "1.2.1-20140415232043-cc305d0",
+                      "net.java.dev.jets3t"  %  "jets3t"          % "0.9.0" )
+  val mundane   = Seq("com.ambiata"          %% "mundane"         % "1.2.1-20140505014353-a175c40")
+
+  def scoobi(version: String) = {
+    val scoobiVersion =
+      if (version.contains("cdh3"))      "0.9.0-cdh3-20140521064448-df53463"
+      else if (version.contains("cdh4")) "0.9.0-cdh4-20140521064733-df53463"
+      else if (version.contains("cdh5")) "0.9.0-cdh5-20140521065009-df53463"
+      else                               "0.9.0-cdh5-20140521065009-df53463"
+
+
+    Seq("com.nicta" %% "scoobi" % scoobiVersion)
+  }
+
+  val resolvers = Seq(
+      Resolver.sonatypeRepo("releases"),
+      Resolver.sonatypeRepo("snapshots"),
+      Resolver.sonatypeRepo("public"),
+      Resolver.typesafeRepo("releases"),
+      "cloudera"             at "https://repository.cloudera.com/content/repositories/releases",
+      Resolver.url("ambiata-oss", new URL("https://ambiata-oss.s3.amazonaws.com"))(Resolver.ivyStylePatterns),
+      "Scalaz Bintray Repo"  at "http://dl.bintray.com/scalaz/releases")
+}
