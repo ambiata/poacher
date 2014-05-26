9053db1c2f3328e871fbef73e3d4cb79c9338108
Eric Torreborre <etorreborre@yahoo.com>
2014-05-26 16:34:42 +1000

    added ScoobiAction classes

---
+++ src/main/scala/com/ambiata/chassflad/scoobi/package.scala
@@ -0,0 +1,12 @@
+package com.ambiata.chassflad
+
+import com.ambiata.chassflad.scoobi.ScoobiAwsAction
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient
+
+package object scoobi {
+  type ScoobiS3Action[A] = ScoobiAwsAction[A, AmazonS3Client]
+  type ScoobiS3EMRAction[A] = ScoobiAwsAction[A, AmazonS3EMRClient]
+  type AmazonEMRClient = AmazonElasticMapReduceClient
+  type AmazonS3EMRClient = (AmazonS3Client, AmazonElasticMapReduceClient)
+}
