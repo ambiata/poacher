82cf384dd63d788675e9ab73633acd456e27a31c
Eric Torreborre <etorreborre@yahoo.com>
2014-07-07 17:37:57 +1000

    updated dependencies

--- project/depend.scala
+++ project/depend.scala
@@ -12,17 +12,20 @@ object depend {
                       "org.specs2"           %% "specs2-junit",
                       "org.specs2"           %% "specs2-html",
                       "org.specs2"           %% "specs2-matcher-extra",
-                      "org.specs2"           %% "specs2-scalacheck").map(_ % "2.3.10")
+                      "org.specs2"           %% "specs2-scalacheck").map(_ % "2.3.12")
   val commonsio = Seq("commons-io"           %  "commons-io"      % "2.4")
   val thrift    = Seq("org.apache.thrift"    %  "libthrift"       % "0.9.1")
-  val mundane   = Seq("com.ambiata"          %% "mundane"         % "1.2.1-20140505014353-a175c40")
+  val mundane   = Seq("com.ambiata"          %% "mundane-control",
+                      "com.ambiata"          %% "mundane-io"     ,
+                      "com.ambiata"          %% "mundane-store"  ,
+                      "com.ambiata"          %% "mundane-testing").map(_ % "1.2.1-20140706115053-2c11cc2")
 
   def scoobi(version: String) = {
-    val scoobiVersion =
-      if (version.contains("cdh3"))      "0.9.0-cdh3-20140521064448-df53463"
-      else if (version.contains("cdh4")) "0.9.0-cdh4-20140521064733-df53463"
-      else if (version.contains("cdh5")) "0.9.0-cdh5-20140521065009-df53463"
-      else                               "0.9.0-cdh5-20140521065009-df53463"
+    val scoobiVersion =                                               
+      if (version.contains("cdh3"))      "0.9.0-cdh3-20140707015433-646465a"
+      else if (version.contains("cdh4")) "0.9.0-cdh4-20140707015150-646465a"
+      else if (version.contains("cdh5")) "0.9.0-cdh5-20140707014808-646465a"
+      else                               "0.9.0-cdh5-20140707014808-646465a"
 
 
     Seq("com.nicta" %% "scoobi" % scoobiVersion)
